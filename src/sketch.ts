9b04989e41d1299f4f647f221bb231b5d325b921
Daniel Bush <dlb.id.au@gmail.com>
2023-01-17 22:08:59 +1100

    feat: we can style custom elements with external stylesheet

--- src/sketch.ts
+++ src/sketch.ts
@@ -5,3 +5,15 @@ document.addEventListener('focusin', (evt) => {
   console.log(evt.relatedTarget, '->', evt.target);
 });
 load(document.body);
+
+class AppDrawer extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<b class="app-drawer">I'm an app-drawer!</b>`;
+  }
+}
+window.customElements.define('app-drawer', AppDrawer);
+
+document.body.insertBefore(
+  document.createElement('app-drawer'),
+  document.body.firstChild,
+);

60f0c6cca3277a258fe188b7ffd0754009c582d5
Daniel Bush <dlb.id.au@gmail.com>
2023-02-07 22:39:35 +1100

    test: coverage

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
   setupFiles: ['<rootDir>/jest.setup.js'],
+  coverageThreshold: {
+    global: {
+      statements: 79,
+      branches: 62,
+      functions: 90,
+      lines: 80,
+    },
+  },
 };

d1d22e58f2398594321603cd84989918cdc72bda
Daniel Bush <dlb.id.au@gmail.com>
2023-03-08 21:21:48 +1100

    chore: bindings

--- src/config/binding.ts
+++ src/config/binding.ts
@@ -2,10 +2,10 @@ import * as action from '../action';
 import { Bindings } from '../binding';
 
 export const defaultBindings: Bindings = [
-  ['j', action.REC_NEXT],
-  ['k', action.REC_PREV],
-  ['ctrl+j', action.SIB_NEXT],
-  ['ctrl+k', action.SIB_PREV],
+  ['ctrl+j', action.REC_NEXT],
+  ['ctrl+k', action.REC_PREV],
+  ['j', action.SIB_NEXT],
+  ['k', action.SIB_PREV],
   ['ctrl+cmd+u', action.UP],
   ['ctrl+alt+u', action.UP],
 ];

0d109f20c3740436165eb80f4be2d47facd1817f
Daniel Bush <dlb.id.au@gmail.com>
2023-03-12 13:50:02 +1100

    chore: tidy up

--- src/focus.ts
+++
@@ -1,7 +0,0 @@
-export function getCurrentFocus(): HTMLElement | null {
-  const ae = document.activeElement;
-  if (ae && ae instanceof window.HTMLElement) {
-    return ae;
-  }
-  return null;
-}

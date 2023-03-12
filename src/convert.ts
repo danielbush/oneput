373121c6c31a3997d38027e0ed8c9474f9bfe2df
Daniel Bush <dlb.id.au@gmail.com>
2023-03-12 13:58:40 +1100

    refactor: convert

--- src/convert.ts
+++ src/convert.ts
@@ -1,12 +1,13 @@
 import MarkdownIt from 'markdown-it';
 import mk from '@iktakahiro/markdown-it-katex';
 
-const md = new MarkdownIt();
-md.use(mk);
-
+/**
+ * Convert markdown content to html but handle katex.
+ */
 function convert(markdown: string): string {
-  const result = md.render(markdown);
-  return result;
+  const md = new MarkdownIt();
+  md.use(mk);
+  return md.render(markdown);
 }
 
 export { convert };

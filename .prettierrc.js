87462efea0ef7507d6c55b9f57f1af20a59798f4
Daniel Bush <dlb.id.au@gmail.com>
2023-12-22 23:22:36 +1100

    chore: eslint, prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all', // other options `es5` or `none`
+  bracketSpacing: true,
+  arrowParens: 'always', // other option 'avoid'
+  // parser: 'typescript',
+  jsxBracketSameLine: false,
+};

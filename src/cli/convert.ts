a85743ea8ac397a6683d2201d749cefe983276c3
Daniel Bush <dlb.id.au@gmail.com>
2023-12-26 13:24:29 +1100

    chore: use commander for cli

--- src/cli/convert.ts
+++ src/cli/convert.ts
@@ -1,17 +1,18 @@
+import { Command } from 'commander';
 import { convert } from '../lib/convert';
 import * as fs from 'fs';
 
-const fileName = process.argv[2];
+const program = new Command();
+program.name('convert').description('Convert 2br markdown to 2br html');
+program
+  .argument('<pathtofile>', 'markdown file to convert')
+  .action((fileName) => {
+    if (!fs.existsSync(fileName)) {
+      console.error(`File "${fileName}" doesn't exist!`);
+      process.exit(1);
+    }
+    const markdown = fs.readFileSync(fileName, 'utf-8');
+    process.stdout.write(convert(markdown, fileName));
+  });
 
-if (!fileName) {
-  console.error('You need to specify a file.');
-  process.exit(1);
-}
-if (!fs.existsSync(fileName)) {
-  console.error(`File "${fileName}" doesn't exist.`);
-  process.exit(1);
-}
-
-const markdown = fs.readFileSync(fileName, 'utf-8');
-
-process.stdout.write(convert(markdown, fileName));
+program.parse();

cebc6edfa42ac0558b903deafa849538267183a1
Daniel Bush <dlb.id.au@gmail.com>
2025-08-20 15:48:27 +1000

    refactor: extract controller to a class, pass in reactive state

--- src/lib/oneput/OneputWrapper.svelte
+++ src/lib/oneput/OneputWrapper.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { tinykeys } from 'tinykeys';
 	import Oneput from './Oneput.svelte';
-	import type { Controller, OneputControllerParams, OneputProps } from './lib.js';
+	import type { OneputProps } from './lib.js';
+	import { Controller } from './controller.js';
 
 	const currentProps = $state<OneputProps>({
 		inputValue: '',
@@ -11,29 +11,7 @@
 		menuOpen: false
 	});
 
-	const handleGlobalKeys = (keys?: OneputControllerParams['globalKeys']) => {
-		if (keys?.keys) {
-			for (const [key, thunk] of Object.entries(keys.keys)) {
-				console.log('setting up key', key, thunk);
-				tinykeys(document.body, { [key]: thunk });
-			}
-		}
-	};
-
-	export const controller: Controller = {
-		update(options: OneputControllerParams) {
-			if (options.input) {
-				currentProps.input = options.input;
-			}
-			handleGlobalKeys(options.globalKeys);
-			if (options.menuOpen !== undefined) {
-				currentProps.menuOpen = options.menuOpen;
-			}
-		},
-		get menuOpen() {
-			return currentProps.menuOpen ?? false;
-		},
-	};
+	const controller = new Controller(currentProps);
 
 	const { controllerRef }: { controllerRef: (c: typeof controller) => void } = $props();
 
---
+++ src/lib/oneput/controller.ts
@@ -0,0 +1,32 @@
+import { tinykeys } from 'tinykeys';
+import type { OneputControllerParams, OneputProps } from './lib.js';
+
+export class Controller {
+	/**
+	 * @param currentProps Should be reactive eg $state<OneputProps>({...})
+	 */
+	constructor(private currentProps: OneputProps) {}
+
+	private handleGlobalKeys(keys?: OneputControllerParams['globalKeys']) {
+		if (keys?.keys) {
+			for (const [key, thunk] of Object.entries(keys.keys)) {
+				console.log('setting up key', key, thunk);
+				tinykeys(document.body, { [key]: thunk });
+			}
+		}
+	}
+
+	update(options: OneputControllerParams) {
+		if (options.input) {
+			this.currentProps.input = options.input;
+		}
+		this.handleGlobalKeys(options.globalKeys);
+		if (options.menuOpen !== undefined) {
+			this.currentProps.menuOpen = options.menuOpen;
+		}
+	}
+
+	get menuOpen() {
+		return this.currentProps.menuOpen ?? false;
+	}
+}

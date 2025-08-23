42b32ec1426198876bb992c130d5de06f390a0bd
Daniel Bush <dlb.id.au@gmail.com>
2025-08-24 07:55:41 +1000

    feat: update menu item builder to support optional icons and add new menu item

--- src/routes/demo/live/+layout.svelte
+++ src/routes/demo/live/+layout.svelte
@@ -14,7 +14,7 @@
 	const piIcon =
 		'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pi-icon lucide-pi"><line x1="9" x2="9" y1="4" y2="20"/><path d="M4 7c0-1.7 1.3-3 3-3h13"/><path d="M18 20c-1.7 0-3-1.3-3-3V4"/></svg>';
 
-	const menuItemWithIcon = ({ id, icon, text }: { id: string; icon: string; text: string }) => {
+	const menuItemWithIcon = ({ id, icon, text }: { id: string; icon?: string; text: string }) => {
 		return {
 			id,
 			type: 'hflex',
@@ -49,7 +49,13 @@
 			// Setting input will show the input part of Oneput.
 			input: {},
 			menu: {
-				items: [menuItemWithIcon({ id: 'insert-katex', icon: piIcon, text: 'Insert katex...' })]
+				items: [
+					menuItemWithIcon({ id: 'insert-katex', icon: piIcon, text: 'Insert katex...' }),
+					menuItemWithIcon({ id: 'some-action-1', text: 'Some action 1...' }),
+					menuItemWithIcon({ id: 'some-action-2', text: 'Some action 2...' }),
+					menuItemWithIcon({ id: 'some-action-3', text: 'Some action 3...' }),
+					menuItemWithIcon({ id: 'some-action-4', text: 'Some action 4...' })
+				]
 			}
 		});
 	};

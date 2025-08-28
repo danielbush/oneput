21fad65f6d3a065d418f8bdd762fa07b97139afd
Daniel Bush <dlb.id.au@gmail.com>
2025-08-28 13:44:09 +1000

    refactor: extract nested ui

--- src/lib/plugins/bindings/ui.ts
+++ src/lib/plugins/bindings/ui.ts
@@ -1,5 +1,41 @@
+import type { OneputControllerParams } from '$lib/oneput/controller.js';
 import { keyboardIcon, menuItemWithIcon, tickIcon, xIcon } from '$lib/ui.js';
 import type { ConfigureBindingsForActionMenu } from './mod.js';
+
+const inputUI = ({ accept, reject }: { accept: () => void; reject: () => void }) =>
+	({
+		placeholder: 'Type the keys...',
+		input: {
+			right: {
+				id: 'input-right-1',
+				type: 'hflex',
+				children: [
+					{
+						id: 'accept-key-capture',
+						tag: 'button',
+						attr: {
+							type: 'button',
+							title: 'Options',
+							onclick: accept
+						},
+						classes: ['oneput__icon-button'],
+						innerHTMLUnsafe: tickIcon
+					},
+					{
+						id: 'reject-key-capture',
+						tag: 'button',
+						attr: {
+							type: 'button',
+							title: 'Options',
+							onclick: reject
+						},
+						classes: ['oneput__icon-button'],
+						innerHTMLUnsafe: xIcon
+					}
+				]
+			}
+		}
+	}) satisfies OneputControllerParams;
 
 /**
  * A menu for managing the key bindings for a given action.
@@ -34,39 +70,7 @@ export const configureBindingsForActionMenu: ConfigureBindingsForActionMenu = (
 				text: 'Add binding...',
 				action: () => {
 					const { accept, reject } = startKeyCapture(actionId);
-					c.update({
-						placeholder: 'Type the keys...',
-						input: {
-							right: {
-								id: 'input-right-1',
-								type: 'hflex',
-								children: [
-									{
-										id: 'accept-key-capture',
-										tag: 'button',
-										attr: {
-											type: 'button',
-											title: 'Options',
-											onclick: accept
-										},
-										classes: ['oneput__icon-button'],
-										innerHTMLUnsafe: tickIcon
-									},
-									{
-										id: 'reject-key-capture',
-										tag: 'button',
-										attr: {
-											type: 'button',
-											title: 'Options',
-											onclick: reject
-										},
-										classes: ['oneput__icon-button'],
-										innerHTMLUnsafe: xIcon
-									}
-								]
-							}
-						}
-					});
+					c.update(inputUI({ accept, reject }));
 				}
 			}),
 			...bindings.map((binding) => {

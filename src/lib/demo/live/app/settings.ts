178b6a8f6d6732de65355279703527bf866ae5a0
Daniel Bush <dlb.id.au@gmail.com>
2025-09-19 15:39:11 +1000

    refactor: extract checkbox to plugins/menu

--- src/lib/demo/live/app/settings.ts
+++ src/lib/demo/live/app/settings.ts
@@ -1,88 +1,10 @@
 import type { Controller } from '$lib/oneput/controller.js';
-import { randomId, type FlexParams, type MenuItem } from '$lib/oneput/lib.js';
 import { KeyBindingsController } from '$lib/oneput/plugins/menu/editBindings.js';
+import { checkboxMenuItem } from '$lib/oneput/plugins/menu/checkboxMenuItem.js';
 import { menuItemWithIcon, type MyDefaultUIValues } from '../config/ui.js';
 import { TestKeyService } from '../service/TestKeyService.js';
 
 const testKeyService = new TestKeyService();
-
-class CheckboxMenuItem implements MenuItem {
-	static create(params: {
-		action: (c: Controller, checked: boolean, node: HTMLInputElement) => void;
-		textContent: string;
-		checked: boolean;
-	}): CheckboxMenuItem {
-		return new CheckboxMenuItem(params);
-	}
-
-	id: string;
-	inputId: string;
-	inputElement?: HTMLInputElement;
-	type = 'hflex' as const;
-	tag = 'button';
-	attr = { type: 'button' };
-	#action: (c: Controller, checked: boolean, node: HTMLInputElement) => void;
-	children: FlexParams['children'];
-
-	constructor(params: {
-		action: (c: Controller, checked: boolean, node: HTMLInputElement) => void;
-		textContent: string;
-		checked: boolean;
-	}) {
-		this.id = randomId();
-		this.inputId = randomId();
-		this.#action = params.action;
-		this.children = [
-			{
-				id: this.inputId,
-				type: 'fchild',
-				tag: 'input',
-				attr: {
-					type: 'checkbox',
-					title: params.textContent,
-					checked: params.checked,
-					onclick: (event: Event) => {
-						event.preventDefault();
-					}
-				},
-				classes: ['oneput__checkbox']
-			},
-			{
-				id: randomId(),
-				type: 'fchild',
-				tag: 'label',
-				attr: {
-					for: this.inputId,
-					onclick: (event: Event) => {
-						event.preventDefault();
-					}
-				},
-				classes: ['oneput__menu-item-body'],
-				textContent: params.textContent
-			}
-		];
-	}
-
-	onMount = (node: HTMLElement) => {
-		this.inputElement = node.querySelector(`#${this.inputId}`) as HTMLInputElement;
-	};
-
-	action = (c: Controller) => {
-		if (!this.inputElement) {
-			return;
-		}
-		this.inputElement.checked = !this.inputElement.checked;
-		this.#action(c, this.inputElement.checked, this.inputElement!);
-	};
-}
-
-function checkboxMenuItem(params: {
-	action: (c: Controller, checked: boolean, node: HTMLInputElement) => void;
-	textContent: string;
-	checked: boolean;
-}): CheckboxMenuItem {
-	return CheckboxMenuItem.create(params);
-}
 
 export const settingsUI = (c: Controller, back: () => void) => {
 	c.setBackBinding(back);
---
+++ src/lib/oneput/plugins/menu/checkboxMenuItem.ts
@@ -0,0 +1,80 @@
+import type { Controller } from '$lib/oneput/controller.js';
+import { randomId, type FlexParams, type MenuItem } from '$lib/oneput/lib.js';
+
+export class CheckboxMenuItem implements MenuItem {
+	static create(params: {
+		action: (c: Controller, checked: boolean, node: HTMLInputElement) => void;
+		textContent: string;
+		checked: boolean;
+	}): CheckboxMenuItem {
+		return new CheckboxMenuItem(params);
+	}
+
+	id: string;
+	inputId: string;
+	inputElement?: HTMLInputElement;
+	type = 'hflex' as const;
+	tag = 'button';
+	attr = { type: 'button' };
+	#action: (c: Controller, checked: boolean, node: HTMLInputElement) => void;
+	children: FlexParams['children'];
+
+	constructor(params: {
+		action: (c: Controller, checked: boolean, node: HTMLInputElement) => void;
+		textContent: string;
+		checked: boolean;
+	}) {
+		this.id = randomId();
+		this.inputId = randomId();
+		this.#action = params.action;
+		this.children = [
+			{
+				id: this.inputId,
+				type: 'fchild',
+				tag: 'input',
+				attr: {
+					type: 'checkbox',
+					title: params.textContent,
+					checked: params.checked,
+					onclick: (event: Event) => {
+						event.preventDefault();
+					}
+				},
+				classes: ['oneput__checkbox']
+			},
+			{
+				id: randomId(),
+				type: 'fchild',
+				tag: 'label',
+				attr: {
+					for: this.inputId,
+					onclick: (event: Event) => {
+						event.preventDefault();
+					}
+				},
+				classes: ['oneput__menu-item-body'],
+				textContent: params.textContent
+			}
+		];
+	}
+
+	onMount = (node: HTMLElement) => {
+		this.inputElement = node.querySelector(`#${this.inputId}`) as HTMLInputElement;
+	};
+
+	action = (c: Controller) => {
+		if (!this.inputElement) {
+			return;
+		}
+		this.inputElement.checked = !this.inputElement.checked;
+		this.#action(c, this.inputElement.checked, this.inputElement!);
+	};
+}
+
+export function checkboxMenuItem(params: {
+	action: (c: Controller, checked: boolean, node: HTMLInputElement) => void;
+	textContent: string;
+	checked: boolean;
+}): CheckboxMenuItem {
+	return CheckboxMenuItem.create(params);
+}

1467bf0ded6fb9f10aaeb2a4d3ecac1a94d2f8c4
Daniel Bush <dlb.id.au@gmail.com>
2025-09-22 15:47:10 +1000

    feat: package exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,8 @@
 // Reexport your entry components here
 import Oneput from './oneput/components/Oneput.svelte';
+import OneputController from './oneput/components/OneputController.svelte';
+import OneputCornerButton from './oneput/components/OneputCornerButton.svelte';
+import Anchor from './oneput/components/Anchor.svelte';
+import { Controller } from './oneput/controller.js';
 
-export { Oneput };
+export { Oneput, OneputController, OneputCornerButton, Anchor, Controller };

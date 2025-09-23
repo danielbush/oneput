95dd4be81743cae3e73608d8688e1282f052eaee
Daniel Bush <dlb.id.au@gmail.com>
2025-09-23 10:36:16 +1000

    chore: add styling and example html from old jsed-ui

--- src/routes/demo/live/+layout.svelte
+++ src/routes/demo/live/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { Anchor, OneputCornerButton, OneputController, Controller } from '$oneput';
+	import '../../../lib/jsed/styles/jsed-defaults.css';
+
 	let { children } = $props();
 	let controller: Controller | null = null;
 	function setController(ctl: Controller) {
@@ -11,6 +13,14 @@
 		window.dispatchEvent(new Event('oneput-toggle-hide'));
 	}, 10);
 </script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.11.1/katex.min.css" />
+	<!-- <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css"
+    /> -->
+</svelte:head>
 
 {@render children()}
 <Anchor>

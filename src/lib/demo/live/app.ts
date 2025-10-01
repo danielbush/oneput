8c42b5ec0f5342b25a9033af16695bd9a403a2ab
Daniel Bush <dlb.id.au@gmail.com>
2025-10-01 14:01:48 +1000

    refactor: extract filter function

--- src/lib/demo/live/app.ts
+++ src/lib/demo/live/app.ts
@@ -1,6 +1,18 @@
 import type { Controller } from '$lib/oneput/controller.js';
 import { MyDefaultUI } from './config/ui.js';
 import { RootUI } from './app/root.js';
+import type { MenuItemAny } from '$lib/oneput/lib.js';
+
+function simpleFilter(input: string, menuItems: MenuItemAny[]) {
+	return menuItems.filter((item) => {
+		return item.children?.some((child) => {
+			if (child.type === 'fchild') {
+				return child.textContent?.toLowerCase().includes(input.toLowerCase());
+			}
+			return false;
+		});
+	});
+}
 
 // Our app starts in this callback.  We get the controller and we can set keys
 // and configure oneput.
@@ -13,15 +25,6 @@ export const setController = (ctl: Controller) => {
 	ctl.ui.setDefaultUI(defaultUI);
 	ctl.keys.setKeys(defaultUI.keys.defaultGlobalKeys, false);
 	ctl.keys.setKeys(defaultUI.keys.defaultLocalKeys, true);
-	ctl.menu.setDefaultMenuItemsFn((input, menuItems) => {
-		return menuItems.filter((item) => {
-			return item.children?.some((child) => {
-				if (child.type === 'fchild') {
-					return child.textContent?.toLowerCase().includes(input.toLowerCase());
-				}
-				return false;
-			});
-		});
-	});
+	ctl.menu.setDefaultMenuItemsFn(simpleFilter);
 	RootUI.create(ctl).run();
 };

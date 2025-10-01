adf7c25930d847ed053581d4c4721adbd02bd49c
Daniel Bush <dlb.id.au@gmail.com>
2025-10-01 15:58:10 +1000

    feat: uFuzzy for menu filtering

--- src/lib/oneput/filters.ts
+++ src/lib/oneput/filters.ts
@@ -1,4 +1,4 @@
-import type { MenuItemAny } from '$lib/oneput/lib.js';
+import type { FChildParams, FlexParams, MenuItemAny } from '$lib/oneput/lib.js';
 import uFuzzy from '@leeoniya/ufuzzy';
 
 export function simpleFilter(input: string, menuItems: MenuItemAny[]) {
@@ -12,13 +12,37 @@ export function simpleFilter(input: string, menuItems: MenuItemAny[]) {
 	});
 }
 
+function walk(item: FlexParams | FChildParams, cb: (item: FlexParams | FChildParams) => void) {
+	if (item.type === 'hflex' || item.type === 'vflex') {
+		for (const child of item.children || []) {
+			walk(child, cb);
+		}
+		return;
+	}
+	if (item.type === 'fchild') {
+		cb(item);
+		return;
+	}
+}
+
+function getTextContent(item: FlexParams | FChildParams) {
+	let result = '';
+	walk(item, (item) => {
+		if (item.type === 'fchild') {
+			if (item.textContent) {
+				result += item.textContent;
+			}
+		}
+	});
+	return result;
+}
+
 const ufuzzy = new uFuzzy({});
 export function fuzzy(input: string, menuItems: MenuItemAny[]) {
-	const haystack = ['foo', 'bar', 'foobar'];
+	const haystack = menuItems.map(getTextContent);
 	const idxs = ufuzzy.filter(haystack, input);
 	if (!idxs) {
-		return [];
+		return menuItems;
 	}
-	console.log(idxs.map((idx) => haystack[idx]));
-	return menuItems;
+	return idxs.map((idx) => menuItems[idx]);
 }

e2a1258c3617c9aae46c41e3eb2596461fd74b9b
Daniel Bush <dlb.id.au@gmail.com>
2025-11-06 16:05:17 +1100

    feat(tomato): slightly better state management

--- src/lib/demo/live/plugins/menu/TomatoTimer.ts
+++ src/lib/demo/live/plugins/menu/TomatoTimer.ts
@@ -17,36 +17,57 @@ export class TomatoTimer {
 		private back: () => void
 	) {}
 
+	private timerIsRunning: boolean = false;
+
+	private startTimer() {
+		this.timerIsRunning = true;
+	}
+
+	private stopTimer() {
+		this.timerIsRunning = false;
+	}
+
 	runUI() {
 		this.ctl.ui.runDefaultUI<MyDefaultUIValues>({
 			menuHeader: 'Tomato Timer',
 			exitAction: this.exit
 		});
+		if (this.timerIsRunning) {
+			this.timerUI();
+		} else {
+			this.noTimerUI();
+		}
+	}
+
+	/**
+	 * The UI we see if there is no existing timer.
+	 */
+	private noTimerUI() {
 		this.ctl.menu.setMenuItems([
 			stdMenuItem({
 				id: 'tomato-start',
 				textContent: 'Start',
 				left: (b) => [b.icon({ innerHTMLUnsafe: icons.playIcon })],
 				action: () => {
-					this.ctl.menu.setMenuItems([
-						stdMenuItem({
-							id: 'tomato-stop',
-							textContent: 'Stop',
-							left: (b) => [b.icon({ innerHTMLUnsafe: icons.stopIcon })],
-							action: () => {
-								this.ctl.menu.setMenuItems([
-									stdMenuItem({
-										id: 'tomato-start',
-										textContent: 'Start',
-										left: (b) => [b.icon({ innerHTMLUnsafe: icons.playIcon })],
-										action: () => {
-											console.log('start');
-										}
-									})
-								]);
-							}
-						})
-					]);
+					this.startTimer();
+					this.runUI();
+				}
+			})
+		]);
+	}
+
+	/**
+	 * The UI we see if there is an existing timer.
+	 */
+	private timerUI() {
+		this.ctl.menu.setMenuItems([
+			stdMenuItem({
+				id: 'tomato-stop',
+				textContent: 'Stop',
+				left: (b) => [b.icon({ innerHTMLUnsafe: icons.stopIcon })],
+				action: () => {
+					this.stopTimer();
+					this.runUI();
 				}
 			})
 		]);

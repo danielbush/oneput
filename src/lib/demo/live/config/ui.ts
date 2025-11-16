2064e68fb57a984ed216742e8a2b488d661ae938
Daniel Bush <dlb.id.au@gmail.com>
2025-11-16 13:35:20 +1100

    refactor: plugins vs shared

--- src/lib/demo/live/config/ui.ts
+++ src/lib/demo/live/config/ui.ts
@@ -9,7 +9,7 @@ import {
 import { arrowLeftIcon, chevronDown, xIcon } from '$lib/oneput/shared/icons.js';
 import { DateDisplay } from '../../../oneput/shared/widgets/DateDisplay.js';
 import { MenuStatus } from '../../../oneput/shared/widgets/MenuStatus/MenuStatus.js';
-import { TimeDisplay } from '../../../oneput/shared/TimeDisplay.js';
+import { TimeDisplay } from '../../../oneput/shared/widgets/TimeDisplay.js';
 import { WordFilter } from '$lib/oneput/filters/WordFilter.js';
 
 /**

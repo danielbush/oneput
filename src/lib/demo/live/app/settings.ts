437f2525260e24a62a9859c2eda0bdc72d4e1620
Daniel Bush <dlb.id.au@gmail.com>
2025-11-20 17:16:07 +1100

    refactor: move TestBindingStore

--- src/lib/demo/live/app/settings.ts
+++ src/lib/demo/live/app/settings.ts
@@ -3,7 +3,7 @@ import { listFilterIcon } from '$lib/oneput/shared/icons.js';
 import { checkboxMenuItem } from '$lib/oneput/shared/checkboxMenuItem.js';
 import { menuItemWithIcon, type MyDefaultUIValues } from '../config/defaultUI.js';
 import { KeysManager } from './KeysManager/KeysManager.js';
-import { config, TestBindingsStore } from '../service/TestBindingsStore.js';
+import { config, TestBindingsStore } from '../../../oneput/shared/TestBindingsStore.js';
 import { FiltersUI } from './FiltersUI.js';
 
 export class SettingsUI {

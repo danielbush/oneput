02fb86eb64f312ade6ea0baa593d512e2fe648ce
Daniel Bush <dlb.id.au@gmail.com>
2025-11-21 12:53:06 +1100

    refactor: extract default key logic

--- src/lib/demo/live/app.ts
+++ src/lib/demo/live/app.ts
@@ -1,13 +1,5 @@
 import type { Controller } from '$lib/oneput/controller.js';
-import {
-	defaultGlobalActions,
-	defaultGlobalBindings,
-	defaultLocalActions,
-	defaultLocalBindings
-} from '$lib/oneput/shared/bindings/defaultBindings.js';
-import { KeyEventBindings } from '$lib/oneput/bindings.js';
-import { BindingsIDB } from '$lib/oneput/shared/bindings/BindingsIDB.js';
-import { MyDefaultUI } from './config/defaultUI.js';
+import { DefaultBindings, MyDefaultUI } from './config/defaultUI.js';
 import { RootUI } from './app/root.js';
 
 // Our app starts in this callback.  We get the controller and we can set keys
@@ -17,29 +9,7 @@ import { RootUI } from './app/root.js';
 // set for us.  But we could fetch settings asynchronously here, update
 // defaultUI accordingly
 export const setController = async (ctl: Controller) => {
-	ctl.ui.setDefaultUI(MyDefaultUI.create(ctl));
-	const bindingsStore = BindingsIDB.create();
-	bindingsStore
-		.getKeys(false, defaultGlobalBindings)
-		.map((kbMapSerializable) => {
-			console.log('getting global keys', kbMapSerializable);
-			const keyBindingMap = KeyEventBindings.fromSerializable(
-				kbMapSerializable,
-				defaultGlobalActions
-			).keyBindingMap;
-			ctl.keys.setDefaultKeys(keyBindingMap, false);
-		})
-		.orTee((err) => ctl.notify(`Error getting global keys: ${err.message}`));
-	bindingsStore
-		.getKeys(true, defaultLocalBindings)
-		.map((kbMapSerializable) => {
-			console.log('getting local keys', kbMapSerializable);
-			const keyBindingMap = KeyEventBindings.fromSerializable(
-				kbMapSerializable,
-				defaultLocalActions
-			).keyBindingMap;
-			ctl.keys.setDefaultKeys(keyBindingMap, true);
-		})
-		.orTee((err) => ctl.notify(`Error getting local keys: ${err.message}`));
+	MyDefaultUI.create(ctl).setDefaultUI();
+	DefaultBindings.create(ctl).setDefaultBindings();
 	ctl.runUI(RootUI);
 };
--- src/lib/demo/live/config/defaultUI.ts
+++ src/lib/demo/live/config/defaultUI.ts
@@ -1,3 +1,11 @@
+import {
+	defaultGlobalActions,
+	defaultGlobalBindings,
+	defaultLocalActions,
+	defaultLocalBindings
+} from '$lib/oneput/shared/bindings/defaultBindings.js';
+import { KeyEventBindings } from '$lib/oneput/bindings.js';
+import { BindingsIDB } from '$lib/oneput/shared/bindings/BindingsIDB.js';
 import type { Controller } from '$lib/oneput/controller.js';
 import { type DefaultUI, type FlexParams, type OneputProps } from '$lib/oneput/lib.js';
 import { arrowLeftIcon, chevronDown, xIcon } from '$lib/oneput/shared/icons.js';
@@ -94,6 +102,10 @@ export class MyDefaultUI<V extends MyDefaultUIValues = MyDefaultUIValues> implem
 		private values: V = {} as V
 	) {}
 
+	setDefaultUI() {
+		this.ctl.ui.setDefaultUI(this);
+	}
+
 	configureUI(values: V) {
 		this.values = {
 			exitAction: () => {
@@ -177,5 +189,42 @@ export class MyDefaultUI<V extends MyDefaultUIValues = MyDefaultUIValues> implem
 				})
 			]
 		});
+	}
+}
+
+export class DefaultBindings {
+	static create(ctl: Controller) {
+		const bindingsStore = BindingsIDB.create();
+		return new DefaultBindings(ctl, bindingsStore);
+	}
+
+	constructor(
+		private ctl: Controller,
+		private bindingsStore: BindingsIDB
+	) {}
+
+	async setDefaultBindings() {
+		this.bindingsStore
+			.getKeys(false, defaultGlobalBindings)
+			.map((kbMapSerializable) => {
+				console.log('getting global keys', kbMapSerializable);
+				const keyBindingMap = KeyEventBindings.fromSerializable(
+					kbMapSerializable,
+					defaultGlobalActions
+				).keyBindingMap;
+				this.ctl.keys.setDefaultKeys(keyBindingMap, false);
+			})
+			.orTee((err) => this.ctl.notify(`Error getting global keys: ${err.message}`));
+		this.bindingsStore
+			.getKeys(true, defaultLocalBindings)
+			.map((kbMapSerializable) => {
+				console.log('getting local keys', kbMapSerializable);
+				const keyBindingMap = KeyEventBindings.fromSerializable(
+					kbMapSerializable,
+					defaultLocalActions
+				).keyBindingMap;
+				this.ctl.keys.setDefaultKeys(keyBindingMap, true);
+			})
+			.orTee((err) => this.ctl.notify(`Error getting local keys: ${err.message}`));
 	}
 }

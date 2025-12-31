fcbb40fdecb0b3fc553a66763cc0ac8ae1c3ef47
Daniel Bush <dlb.id.au@gmail.com>
2026-01-01 10:10:30 +1100

    feat: fallback icon

--- src/lib/oneput/lib/icons.ts
+++ src/lib/oneput/lib/icons.ts
@@ -44,11 +44,10 @@ export function getIconSource(name: string): IconSource | undefined {
  * Used internally by FChild.svelte when an icon prop is specified.
  */
 export function renderIcon(name: string, target: HTMLElement): void {
-	const source = iconRegistry.get(name);
-	console.log('renderIcon', source, name, target);
+	let source = iconRegistry.get(name);
 	if (!source) {
 		console.warn(`Icon "${name}" not found in registry`);
-		return;
+		source = missingIcon;
 	}
 
 	target.innerHTML = ''; // Clear existing content
@@ -58,7 +57,6 @@ export function renderIcon(name: string, target: HTMLElement): void {
 			target.innerHTML = source.svg;
 			break;
 		case 'lucide':
-			console.log('lucide', source.icon());
 			target.appendChild(source.icon());
 			break;
 		case 'element':
@@ -82,3 +80,7 @@ export function lucide(icon: () => SVGElement): IconSource {
 export function svg(svgString: string): IconSource {
 	return { type: 'svg', svg: svgString };
 }
+
+export const missingIcon = svg(
+	'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-image-off-icon lucide-image-off"><line x1="2" x2="22" y1="2" y2="22"/><path d="M10.41 10.41a2 2 0 1 1-2.83-2.83"/><line x1="13.5" x2="6" y1="13.5" y2="21"/><line x1="18" x2="21" y1="12" y2="15"/><path d="M3.59 3.59A1.99 1.99 0 0 0 3 5v14a2 2 0 0 0 2 2h14c.55 0 1.052-.22 1.41-.59"/><path d="M21 15V5a2 2 0 0 0-2-2H9"/></svg>'
+);

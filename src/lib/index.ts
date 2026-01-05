f0df54ed2188d174fb500ce94fa572fab7a62850
Daniel Bush <dlb.id.au@gmail.com>
2026-01-05 23:21:04 +1100

    chore: shuffling deck chairs on the index barrel file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,21 +1,18 @@
-// Reexport your entry components here
-import Oneput from './oneput/components/Oneput.svelte';
-import OneputController from './oneput/components/OneputController.svelte';
-import OneputCornerButton from './oneput/components/OneputCornerButton.svelte';
-import Anchor from './oneput/components/Anchor.svelte';
-import { Controller } from './oneput/controllers/controller.js';
-import type { UILayout } from './oneput/types.js';
+// Components
+export { default as Oneput } from './oneput/components/Oneput.svelte';
+export { default as OneputController } from './oneput/components/OneputController.svelte';
+export { default as OneputCornerButton } from './oneput/components/OneputCornerButton.svelte';
+export { default as Anchor } from './oneput/components/Anchor.svelte';
 
-// Icon registry exports
-export {
-	registerIcon,
-	registerIcons,
-	getIconRenderer,
-	renderIcon,
-	element,
-	missingIcon
-} from './oneput/lib/icons.js';
+// Controllers
+export { Controller } from './oneput/controllers/controller.js';
+
+// Types
+export * from './oneput/types.js';
+
+// Lib
+export { registerIcon, registerIcons, element, unsafeHTML } from './oneput/lib/icons.js';
 export type { IconRenderer } from './oneput/lib/icons.js';
 
-export { Oneput, OneputController, OneputCornerButton, Anchor, Controller };
-export type { UILayout };
+// Shared
+// TODO: Separate exports entry?

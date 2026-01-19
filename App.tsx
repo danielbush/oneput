3d38462ec21fa06a97b02a4a6e8e5891347cd2f4
Daniel Bush <dlb.id.au@gmail.com>
2026-01-19 01:52:27 +0000

    chore: tokenize html

--- App.tsx
+++ App.tsx
@@ -24,8 +24,18 @@ const loremIpsumHTML = `
   ${Array.from(
     { length: 20 },
     (_, i) =>
-      `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>`
+      `<p>Lorem! ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>`
   ).join('')}
+  <script>
+    (function() {
+      const firstParagraph = document.querySelector('p');
+      if (firstParagraph) {
+        const text = firstParagraph.textContent;
+        const words = text.split(/\s+/).filter(word => word.length > 0);
+        firstParagraph.innerHTML = words.map(word => '<span>' + word + '</span>').join(' ');
+      }
+    })();
+  </script>
 </body>
 </html>
 `;

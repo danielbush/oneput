e69955e6f72a0581341f2ff5d3d0fd3a9d6f5283
Daniel Bush <dlb.id.au@gmail.com>
2026-02-12 14:39:45 +1100

    refactor(jsed): group actions

--- packages/jsed/src/lib/oneput/app/_actions.ts
+++ packages/jsed/src/lib/oneput/app/_actions.ts
@@ -14,42 +14,9 @@ export const actions: Record<string, (ctl: Controller) => void> = {
   hideOneput: (ctl) => {
     ctl.toggleHide();
   },
-  loadTestDoc: async (ctl) => {
-    const docRoot = document.getElementById('load-doc');
-    if (!docRoot) {
-      ctl.notify('Could not load test doc!');
-      return;
-    }
 
-    try {
-      const response = await fetch('/api/docs/test_doc');
-      if (!response.ok) {
-        ctl.notify('Failed to load test doc!');
-        return;
-      }
-      const html = await response.text();
-      state.currentDocument = Document.createFromHTML(docRoot, html);
-      ctl.menu.closeMenu();
-    } catch (err) {
-      ctl.notify('Error loading test doc!');
-      console.error(err);
-    }
-  },
-  REC_NEXT: () => {
-    state.currentDocument?.document.nav.REC_NEXT();
-  },
-  REC_PREV: () => {
-    state.currentDocument?.document.nav.REC_PREV();
-  },
-  SIB_NEXT: () => {
-    state.currentDocument?.document.nav.SIB_NEXT();
-  },
-  SIB_PREV: () => {
-    state.currentDocument?.document.nav.SIB_PREV();
-  },
-  UP: () => {
-    state.currentDocument?.document.nav.UP();
-  },
+  // App state transitions (ctl.app.run(AppObject)).
+
   EDIT_FIRST: (ctl) => {
     if (state?.currentDocument?.document) {
       const focus = state.currentDocument.document.nav.getFocus();
@@ -64,6 +31,47 @@ export const actions: Record<string, (ctl: Controller) => void> = {
         }
       }
     }
+  },
+
+  // Menu actions.
+
+  loadTestDoc: async (ctl) => {
+    const docRoot = document.getElementById('load-doc');
+    if (!docRoot) {
+      ctl.notify('Could not load test doc!');
+      return;
+    }
+    try {
+      const response = await fetch('/api/docs/test_doc');
+      if (!response.ok) {
+        ctl.notify('Failed to load test doc!');
+        return;
+      }
+      const html = await response.text();
+      state.currentDocument = Document.createFromHTML(docRoot, html);
+      ctl.menu.closeMenu();
+    } catch (err) {
+      ctl.notify('Error loading test doc!');
+      console.error(err);
+    }
+  },
+
+  // Editor actions
+
+  REC_NEXT: () => {
+    state.currentDocument?.document.nav.REC_NEXT();
+  },
+  REC_PREV: () => {
+    state.currentDocument?.document.nav.REC_PREV();
+  },
+  SIB_NEXT: () => {
+    state.currentDocument?.document.nav.SIB_NEXT();
+  },
+  SIB_PREV: () => {
+    state.currentDocument?.document.nav.SIB_PREV();
+  },
+  UP: () => {
+    state.currentDocument?.document.nav.UP();
   },
   TOGGLE_SELECT: (ctl) => {
     ctl.input.toggleSelect();

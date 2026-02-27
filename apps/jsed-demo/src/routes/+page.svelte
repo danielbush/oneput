59b67c95ed897405e486bf6fbd752038ed470176
Daniel Bush <dlb.id.au@gmail.com>
2026-02-27 19:54:33 +1100

    refactor(jsed-demo): put demo in root route

--- apps/jsed-demo/src/routes/+page.svelte
+++ apps/jsed-demo/src/routes/+page.svelte
@@ -1,8 +1,4 @@
-<script>
-  import { resolve } from '$app/paths';
+<script lang="ts">
 </script>
 
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>Visit <a href={resolve('/demo')}>/demo</a> for jsed demo</p>
+<div id="load-doc"></div>

d723221f118df50c08758425e6c56b5c88c94f95
Daniel Bush <dlb.id.au@gmail.com>
2026-02-27 15:10:51 +1100

    refactor: safeFetch -> Fetch

--- apps/jsed-demo/src/lib/jsed/services/TestDocService.ts
+++ apps/jsed-demo/src/lib/jsed/services/TestDocService.ts
@@ -1,26 +1,28 @@
-import type { Controller } from '@oneput/oneput';
 import { errAsync, ResultAsync, err, ok } from 'neverthrow';
 
 export type FetchError =
   | { type: 'network'; cause: Error }
   | { type: 'http'; status: number; statusText: string };
 
-/**
- * Use neverthrow.  Returns an Err if the fetch fails or status is not OK.
- */
-function safeFetch(input: RequestInfo | URL, init?: RequestInit) {
-  return ResultAsync.fromPromise(
-    fetch(input, init),
-    (e): FetchError => ({ type: 'network', cause: e as Error })
-  ).andThen((response) =>
-    response.ok
-      ? ok(response)
-      : err<Response, FetchError>({
-          type: 'http',
-          status: response.status,
-          statusText: response.statusText
-        })
-  );
+class Fetch {
+  static create() {
+    return new Fetch();
+  }
+
+  fetch = (input: RequestInfo | URL, init?: RequestInit) => {
+    return ResultAsync.fromPromise(
+      fetch(input, init),
+      (e): FetchError => ({ type: 'network', cause: e as Error })
+    ).andThen((response) =>
+      response.ok
+        ? ok(response)
+        : err<Response, FetchError>({
+            type: 'http',
+            status: response.status,
+            statusText: response.statusText
+          })
+    );
+  };
 }
 
 export type TestDocError = { type: 'missing-element'; id: string };
@@ -30,17 +32,16 @@ export type TestDocError = { type: 'missing-element'; id: string };
  */
 export class TestDocService {
   static create() {
-    return new TestDocService({ fetch: safeFetch });
+    return new TestDocService(Fetch.create().fetch);
   }
-  constructor(private params: { fetch: typeof safeFetch }) {}
+  constructor(private fetch: Fetch['fetch']) {}
 
   loadTestDoc(): ResultAsync<HTMLElement, TestDocError | FetchError> {
     const docRoot = document.getElementById('load-doc');
     if (!docRoot) {
       return errAsync({ type: 'missing-element' as const, id: 'load-doc' });
     }
-    return this.params
-      .fetch('/api/docs/test_doc')
+    return this.fetch('/api/docs/test_doc')
       .andThen((response) =>
         ResultAsync.fromPromise(
           response.text(),

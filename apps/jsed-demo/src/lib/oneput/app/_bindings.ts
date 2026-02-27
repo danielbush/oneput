f2f730954d2ceab7a967c3c7333344e351d5a44f
Daniel Bush <dlb.id.au@gmail.com>
2026-02-27 13:35:15 +1100

    refactor(jsed-demo): _bindings

---
+++ apps/jsed-demo/src/lib/oneput/app/_bindings.ts
@@ -0,0 +1,140 @@
+import { bindings } from '@oneput/oneput';
+import type { Controller, KeyBindingMapSerializable } from '@oneput/oneput';
+import { state } from '../state.js';
+
+export const defaultGlobalBindings: KeyBindingMapSerializable = {
+  openMenu: {
+    bindings: ['$mod+b'],
+    description: 'Open Oneput menu...'
+  },
+  focusInput: {
+    bindings: ['$mod+[', 'Control+['],
+    description: 'Focus input'
+  },
+  hideOneput: {
+    bindings: ['$mod+h'],
+    description: 'Hide Oneput'
+  },
+  REC_NEXT: {
+    bindings: ['$mod+Shift+j', 'Shift+ArrowDown'],
+    description: 'Navigate to next element'
+  },
+  REC_PREV: {
+    bindings: ['$mod+Shift+k', 'Shift+ArrowUp'],
+    description: 'Navigate to previous element'
+  },
+  SIB_NEXT: { bindings: ['$mod+j', 'ArrowDown'], description: 'Navigate to next sibling' },
+  SIB_PREV: { bindings: ['$mod+k', 'ArrowUp'], description: 'Navigate to previous sibling' },
+  UP: { bindings: ['$mod+u', '$mod+ArrowUp'], description: 'Find next parent' },
+  EDIT_FIRST: { bindings: ['enter'], description: 'Edit first editable token' },
+  TOGGLE_SELECT: { bindings: ['$mod+e'], description: 'Toggle input element cursor state' }
+};
+
+export const defaultGlobalActions: Record<string, (c: Controller) => void> = {
+  openMenu: (ctl) => {
+    ctl.menu.openMenu();
+  },
+  focusInput: (ctl) => {
+    ctl.input.focusInput();
+  },
+  hideOneput: (ctl) => {
+    ctl.toggleHide();
+  },
+
+  // Editor actions
+
+  REC_NEXT: () => {
+    state.currentDocument?.nav.REC_NEXT();
+  },
+  REC_PREV: () => {
+    state.currentDocument?.nav.REC_PREV();
+  },
+  SIB_NEXT: () => {
+    state.currentDocument?.nav.SIB_NEXT();
+  },
+  SIB_PREV: () => {
+    state.currentDocument?.nav.SIB_PREV();
+  },
+  UP: () => {
+    state.currentDocument?.nav.UP();
+  },
+  TOGGLE_SELECT: (ctl) => {
+    ctl.input.toggleSelect();
+  }
+};
+
+export const globalKeys = bindings.KeyEventBindings.fromSerializable(
+  defaultGlobalBindings,
+  defaultGlobalActions
+).keyBindingMap;
+
+export const defaultLocalActions: Record<string, (c: Controller) => void> = {
+  hideOneput: (c) => {
+    c.toggleHide();
+  },
+  doAction: (c) => {
+    c.menu.doMenuAction();
+  },
+  back: (c) => {
+    c.app.goBack();
+  },
+  focusInput: (c) => {
+    c.input.focusInput();
+  },
+  closeMenu: (c) => {
+    c.menu.closeMenu();
+  },
+  focusPreviousMenuItem: (c) => {
+    c.menu.focusPreviousMenuItem();
+  },
+  focusNextMenuItem: (c) => {
+    c.menu.focusNextMenuItem();
+  },
+  fill: (c) => {
+    c.menu.runFillHandler();
+  },
+  submit: (c) => {
+    c.input.runSubmitHandler();
+  }
+};
+
+export const defaultLocalBindings: KeyBindingMapSerializable = {
+  hideOneput: {
+    bindings: [],
+    description: 'Hide Oneput'
+  },
+  doAction: {
+    bindings: ['Enter'],
+    description: 'Do action'
+  },
+  submit: {
+    bindings: ['$mod+Enter'],
+    description: 'Submit input'
+  },
+  // NOTE: reserve 'Shift+Enter' for newlines in text area input.
+  back: {
+    bindings: ['Meta+B'],
+    description: 'Back'
+  },
+  focusInput: {
+    bindings: ['$mod+[', 'Control+['],
+    description: 'Focus input'
+  },
+  closeMenu: {
+    bindings: ['$mod+b', 'Escape'],
+    description: 'Close menu'
+  },
+  focusPreviousMenuItem: {
+    bindings: ['$mod+k'],
+    description: 'Focus previous menu item'
+  },
+  focusNextMenuItem: {
+    bindings: ['$mod+j'],
+    description: 'Focus next menu item'
+  }
+};
+
+export const localKeys = bindings.KeyEventBindings.fromSerializable(
+  defaultLocalBindings,
+  defaultLocalActions
+).keyBindingMap;
--- apps/jsed-demo/src/lib/oneput/app/_layout.ts
+++ apps/jsed-demo/src/lib/oneput/app/_layout.ts
@@ -1,11 +1,10 @@
-import { hflex, utils, bindings } from '@oneput/oneput';
+import { hflex, utils } from '@oneput/oneput';
 import type {
   Controller,
   DynamicPlaceholderBase,
   UILayout,
   FlexChildBuilder,
-  FChildParams,
-  KeyBindingMapSerializable
+  FChildParams
 } from '@oneput/oneput';
 import { DynamicPlaceholder } from '@oneput/oneput/shared/ui/DynamicPlaceholder.js';
 import { WordFilter } from '@oneput/oneput/shared/filters/WordFilter.js';
@@ -13,7 +12,7 @@ import { TimeDisplay } from '@oneput/oneput/shared/components/TimeDisplay.js';
 import { DateDisplay } from '@oneput/oneput/shared/components/DateDisplay.js';
 import MenuStatus from '@oneput/oneput/shared/components/MenuStatus.svelte';
 import { icons } from '../icons.js';
-import { actions } from './_actions.js';
+import { globalKeys, localKeys } from './_bindings.js';
 
 /**
  * Define settings used by your particular layout.
@@ -25,112 +24,6 @@ export type LayoutSettings = {
    */
   outerRight?: (b: FlexChildBuilder) => FChildParams;
 };
-
-export const defaultGlobalBindings: KeyBindingMapSerializable = {
-  openMenu: {
-    bindings: ['$mod+b'],
-    description: 'Open Oneput menu...'
-  },
-  focusInput: {
-    bindings: ['$mod+[', 'Control+['],
-    description: 'Focus input'
-  },
-  hideOneput: {
-    bindings: ['$mod+h'],
-    description: 'Hide Oneput'
-  },
-  REC_NEXT: {
-    bindings: ['$mod+Shift+j', 'Shift+ArrowDown'],
-    description: 'Navigate to next element'
-  },
-  REC_PREV: {
-    bindings: ['$mod+Shift+k', 'Shift+ArrowUp'],
-    description: 'Navigate to previous element'
-  },
-  SIB_NEXT: { bindings: ['$mod+j', 'ArrowDown'], description: 'Navigate to next sibling' },
-  SIB_PREV: { bindings: ['$mod+k', 'ArrowUp'], description: 'Navigate to previous sibling' },
-  UP: { bindings: ['$mod+u', '$mod+ArrowUp'], description: 'Find next parent' },
-  EDIT_FIRST: { bindings: ['enter'], description: 'Edit first editable token' },
-  TOGGLE_SELECT: { bindings: ['$mod+e'], description: 'Toggle input element cursor state' }
-};
-
-export const defaultGlobalActions: Record<string, (c: Controller) => void> = actions;
-
-const globalKeys = bindings.KeyEventBindings.fromSerializable(
-  defaultGlobalBindings,
-  defaultGlobalActions
-).keyBindingMap;
-
-export const defaultLocalActions: Record<string, (c: Controller) => void> = {
-  hideOneput: (c) => {
-    c.toggleHide();
-  },
-  doAction: (c) => {
-    c.menu.doMenuAction();
-  },
-  back: (c) => {
-    c.app.goBack();
-  },
-  focusInput: (c) => {
-    c.input.focusInput();
-  },
-  closeMenu: (c) => {
-    c.menu.closeMenu();
-  },
-  focusPreviousMenuItem: (c) => {
-    c.menu.focusPreviousMenuItem();
-  },
-  focusNextMenuItem: (c) => {
-    c.menu.focusNextMenuItem();
-  },
-  fill: (c) => {
-    c.menu.runFillHandler();
-  },
-  submit: (c) => {
-    c.input.runSubmitHandler();
-  }
-};
-
-export const defaultLocalBindings: KeyBindingMapSerializable = {
-  hideOneput: {
-    bindings: [],
-    description: 'Hide Oneput'
-  },
-  doAction: {
-    bindings: ['Enter'],
-    description: 'Do action'
-  },
-  submit: {
-    bindings: ['$mod+Enter'],
-    description: 'Submit input'
-  },
-  // NOTE: reserve 'Shift+Enter' for newlines in text area input.
-  back: {
-    bindings: ['Meta+B'],
-    description: 'Back'
-  },
-  focusInput: {
-    bindings: ['$mod+[', 'Control+['],
-    description: 'Focus input'
-  },
-  closeMenu: {
-    bindings: ['$mod+b', 'Escape'],
-    description: 'Close menu'
-  },
-  focusPreviousMenuItem: {
-    bindings: ['$mod+k'],
-    description: 'Focus previous menu item'
-  },
-  focusNextMenuItem: {
-    bindings: ['$mod+j'],
-    description: 'Focus next menu item'
-  }
-};
-
-export const localKeys = bindings.KeyEventBindings.fromSerializable(
-  defaultLocalBindings,
-  defaultLocalActions
-).keyBindingMap;
 
 /**
  * Defines a standard layout.

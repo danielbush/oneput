62dd966b18e83635c5f666f03dbb190323d6f18d
Daniel Bush <dlb.id.au@gmail.com>
2026-02-28 17:08:34 +1100

    feat(jsed-demo): pass cursor to EditDocument

--- apps/jsed-demo/src/lib/oneput/app/EditDocument.ts
+++ apps/jsed-demo/src/lib/oneput/app/EditDocument.ts
@@ -1,37 +1,20 @@
 import type { AppObject, Controller } from '@oneput/oneput';
-import * as jsed from '@oneput/jsed';
+import { type IJsedCursor } from '@oneput/jsed';
 
 /**
  * Oneput AppObject that manages an edit session for a single document.
  */
 export class EditDocument implements AppObject {
-  static create(ctl: Controller, params: { document: jsed.JsedDocument }) {
-    return new EditDocument(ctl, params.document);
+  static create(ctl: Controller, params: { cursor: IJsedCursor }) {
+    return new EditDocument(ctl, params.cursor);
   }
 
   constructor(
     private ctl: Controller,
-    private document: jsed.JsedDocument
+    private cursor: IJsedCursor
   ) {}
 
-  public onStart = () => {
-    this.document
-      .requestCursorUnderFocus({
-        onTokenChange: () => {}
-      })
-      .mapErr((err) => {
-        switch (err.type) {
-          case 'no-token-under-focus':
-            this.ctl.notify('No token under focus', { duration: 3000 });
-            break;
-          case 'no-focus':
-            this.ctl.notify('No document focus found', { duration: 3000 });
-            break;
-        }
-      });
-  };
+  public onStart = () => {};
 
-  public onExit = () => {
-    //
-  };
+  public onExit = () => {};
 }
--- apps/jsed-demo/src/lib/oneput/app/ViewDocument.ts
+++ apps/jsed-demo/src/lib/oneput/app/ViewDocument.ts
@@ -1,21 +1,22 @@
 import type { AppObject, Controller } from '@oneput/oneput';
 import { icons } from '../icons.js';
-import * as jsed from '@oneput/jsed';
+import { type IJsedCursor, type JsedDocument } from '@oneput/jsed';
 import { setDocument } from './_bindings.js';
 import { stdMenuItem } from '@oneput/oneput/shared/ui/menuItems/stdMenuItem.js';
 import { EditDocument } from './EditDocument.js';
+// import { EditDocument } from './EditDocument.js';
 
 /**
  * Oneput AppObject that manages a single JsedDocument.
  */
 export class ViewDocument implements AppObject {
-  static create(ctl: Controller, params: { document: jsed.JsedDocument }) {
+  static create(ctl: Controller, params: { document: JsedDocument }) {
     return new ViewDocument(ctl, params.document);
   }
 
   constructor(
     private ctl: Controller,
-    private document: jsed.JsedDocument
+    private document: JsedDocument
   ) {}
 
   onStart = () => {
@@ -27,13 +28,28 @@ export class ViewDocument implements AppObject {
   };
 
   actions = {
-    EDIT_FIRST: () => {
-      this.ctl.app.run(
-        EditDocument.create(this.ctl, {
-          document: this.document
+    EDIT_FIRST: () =>
+      this.document
+        .requestCursorUnderFocus({
+          onTokenChange: () => {}
         })
-      );
-    }
+        .map((cursor) => {
+          this.ctl.app.run(
+            EditDocument.create(this.ctl, {
+              cursor: cursor
+            })
+          );
+        })
+        .mapErr((err) => {
+          switch (err.type) {
+            case 'no-token-under-focus':
+              this.ctl.notify('No token under focus', { duration: 3000 });
+              break;
+            case 'no-focus':
+              this.ctl.notify('No document focus found', { duration: 3000 });
+              break;
+          }
+        })
   };
 
   menu = {

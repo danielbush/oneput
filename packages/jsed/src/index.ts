76711634155b3f30684d8a2efec78d13081473ae
Daniel Bush <dlb.id.au@gmail.com>
2026-05-12 10:16:11 +1000

    chore(jsed): simplify exports

--- packages/jsed/src/index.ts
+++ packages/jsed/src/index.ts
@@ -6,37 +6,3 @@ export { Nav, type OnRequestFocus } from './Nav.js';
 export { Cursor } from './Cursor.js';
 export { Tokenizer } from './Tokenizer.js';
 export { Detokenizer } from './lib/dom/Detokenizer.js';
-export { tokenizeLineAt as tokenizeLine } from './lib/dom/tokenize.js';
-
-// TODO: hmm, should we just provide several entry points in package.json "exports"?
-import * as token from './lib/dom/token.js';
-import { isToken, isAnchor } from './lib/dom/taxonomy.js';
-import { getLine } from './lib/dom/line.js';
-import * as dom from './lib/dom/focusable.js';
-import * as domRules from './lib/dom/dom-rules.js';
-const utils = {
-  dom: {
-    createElement: dom.createElement,
-    insertAfter: dom.insertNewAfter,
-    insertBefore: dom.insertNewBefore,
-    deleteElement: dom.deleteElement,
-    splitParentBefore: dom.splitParentBefore,
-    rules: domRules
-  },
-  token: {
-    getValue: token.getValue,
-    getLine,
-    isToken,
-    isAnchor,
-    addAnchors: token.addAnchors,
-    getParent: token.getParent
-  }
-};
-
-export {
-  JSED_ANCHOR_CLASS,
-  JSED_TOKEN_CLASS,
-  JSED_APP_ROOT_ID,
-  JSED_IGNORE_CLASS
-} from './lib/dom/constants.js';
-export { utils };

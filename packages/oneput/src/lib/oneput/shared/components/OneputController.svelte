c1238677f5b2a68c43710c03bcc647342da81209
Daniel Bush <dlb.id.au@gmail.com>
2026-05-21 20:45:50 +1000

    chore(oneput): make R in AppObject inferred for OneputController from its run prop

--- packages/oneput/src/lib/oneput/shared/components/OneputController.svelte
+++ packages/oneput/src/lib/oneput/shared/components/OneputController.svelte
@@ -1,4 +1,7 @@
-<script lang="ts">
+<!-- typescript infers R based on the `run` prop's return type which allows us
+to infer R from the particular AppObject it returns.  This is similar to a
+function generic type inferred from its arguments. -->
+<script lang="ts" generics="R = unknown">
   // Wraps Oneput and creates and exposes a controller that lets you
   // programmatically control Oneput.
   import Oneput from './Oneput.svelte';
@@ -15,7 +18,7 @@
     menuItems: [],
     menuOpen: false
   });
-  const props: { run: (ctl: Controller) => AppObject } = $props();
+  const props: { run: (ctl: Controller) => AppObject<R> } = $props();
 
   const controller = Controller.create(currentProps);
 

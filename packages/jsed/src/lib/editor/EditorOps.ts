51f99a3c68fee69e5150c0adb1cc4fdd8568984c
Daniel Bush <dlb.id.au@gmail.com>
2026-05-21 11:44:13 +1000

    chore(jsed): todo on fixAnchors (undo work)

--- packages/jsed/src/lib/editor/EditorOps.ts
+++ packages/jsed/src/lib/editor/EditorOps.ts
@@ -457,6 +457,7 @@ export class EditorOps {
   };
 }
 
+// TODO: put next to addAnchors() or use addAnchors()?
 function fixAnchors(el: HTMLElement) {
   if (isToken(el)) {
     const next = getNextElementSibling(el);

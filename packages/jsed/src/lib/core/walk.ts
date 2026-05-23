50e80c548b667d922bf56caad1e8c2216200c538
Daniel Bush <dlb.id.au@gmail.com>
2026-05-24 08:04:43 +1000

    chore(jsed): walk docs

--- packages/jsed/src/lib/core/walk.ts
+++ packages/jsed/src/lib/core/walk.ts
@@ -1,5 +1,10 @@
 export type Walk2Params = {
-  /** Pre-order visit condition.  Only yield nodes that pass this check. Does not control descent. */
+  /**
+   * Pre-order visit condition wrt to the direction of walking.
+   *
+   * If true, the node will be yield by the iterator.
+   * Only yield nodes that pass this check. Does not control descent.
+   */
   visit?: (node: Node) => boolean;
   /** Controls whether to descend into a node's children. Independent of visit. */
   descend?: (node: Node) => boolean;
@@ -9,6 +14,9 @@ export type Walk2Params = {
   visitCeiling?: boolean;
 };
 
+/**
+ * Whether to pre-order visit the node.
+ */
 export function shouldVisit(node: Node, params?: Walk2Params): boolean {
   return !params?.visit || params.visit(node);
 }
